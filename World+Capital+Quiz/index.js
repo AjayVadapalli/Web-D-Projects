d57540f94a72e821d67783717adb09ab04e31436
“AjayVadapalli” <vajaysivasurya@gmail.com>
2025-02-27 15:07:27 +0530

    change

--- World+Capital+Quiz/index.js
+++ World+Capital+Quiz/index.js
@@ -1,18 +1,21 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "World",
-  password: "#Solo143",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
-
 db.connect();
 
 let quiz = [
